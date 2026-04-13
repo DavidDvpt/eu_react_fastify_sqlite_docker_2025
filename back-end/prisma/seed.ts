b10925cd329efa3f49a9a4f012dc60111434dd8c
david mosca <david.mosca@pricehubble.com>
2026-04-13 17:53:28 +0200

    add sessions buy

--- back-end/prisma/seed.ts
+++ back-end/prisma/seed.ts
@@ -6,6 +6,8 @@ import prismaClient from './prismaClient.js';
 import { ITEM_CATEGORIES } from './seedDatas/item_categories.js';
 import { ITEM_TYPES } from './seedDatas/item_types.js';
 import { ITEMS } from './seedDatas/items.js';
+import { INVENTORY_LOTS_SESSION } from './seedDatas/inventory_lots_session.js';
+import { SESSIONS_TRADE, SESSION_TRADE_LINES } from './seedDatas/session_trade.js';
 import { USERS } from './seedDatas/user.js';
 
 const userRepository = new UserRepository(prismaClient);
@@ -41,5 +43,29 @@ const items = await itemRepository.findMany();
 if (!items.length) {
   ITEMS.forEach(async (e) => {
     await itemRepository.create({ data: e });
+  });
+}
+
+const sessionsCount = await prismaClient.session.count();
+if (!sessionsCount) {
+  await prismaClient.session.createMany({
+    data: SESSIONS_TRADE,
+    skipDuplicates: true,
+  });
+}
+
+const sessionLinesCount = await prismaClient.sessionLine.count();
+if (!sessionLinesCount) {
+  await prismaClient.sessionLine.createMany({
+    data: SESSION_TRADE_LINES,
+    skipDuplicates: true,
+  });
+}
+
+const inventoryLotsCount = await prismaClient.inventoryLot.count();
+if (!inventoryLotsCount) {
+  await prismaClient.inventoryLot.createMany({
+    data: INVENTORY_LOTS_SESSION,
+    skipDuplicates: true,
   });
 }
