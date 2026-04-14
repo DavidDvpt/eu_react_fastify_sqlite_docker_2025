b7b0e20697b2dd8073c11043426f83f58f10e917
david mosca <david.mosca@pricehubble.com>
2026-04-15 01:15:34 +0200

    update navbar css

--- front-end/src/components/navbar/Profile.tsx
+++ front-end/src/components/navbar/Profile.tsx
@@ -4,6 +4,7 @@ import { authMeThunk } from "@/modules/auth";
 import logoutApi from "@/modules/auth/services/network/logoutApi";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { useNavigate } from "react-router-dom";
+import { Button } from "../ui/button";
 
 function Profile() {
   const [isOpen, setIsOpen] = useState(false);
@@ -50,18 +51,18 @@ function Profile() {
       ref={containerRef}
       className="w-[200px] flex items-center justify-end p-2 relative bg-layout-app-bg"
     >
-      <button
+      <Button
         type="button"
-        className="inline-flex items-center gap-2 rounded-md border-0 bg-layout-app-bg px-2 py-1 transition-colors cursor-pointer "
+        className="inline-flex items-center gap-2 rounded-md border-0 bg-bg hover:bg-bg px-2 py-1 transition-colors cursor-pointer "
         aria-haspopup="menu"
         aria-expanded={isOpen}
         onClick={() => setIsOpen((prev) => !prev)}
       >
-        <span className="w-8 h-8 rounded-full border border-info/40 bg-info text-info-foreground inline-flex items-center justify-center text-sm font-bold">
+        <span className="w-8 h-8 rounded-full border border-button-primary-border bg-button-primary-bg text-button-primary-text inline-flex items-center justify-center text-sm font-bold">
           {avatarLetter}
         </span>
-        <ChevronDown className="h-4 w-4 text-foreground " />
-      </button>
+        <ChevronDown className="h-4 w-4 text-white" />
+      </Button>
 
       {isOpen ? (
         <div className="absolute right-2 top-full mt-1 w-40 p-1 shadow-ambient-lg z-20 bg-layout-app-bg">
@@ -74,7 +75,7 @@ function Profile() {
           </button>
           <button
             type="button"
-            className="w-full rounded-sm px-3 py-2 text-left text-sm text-danger hover:bg-danger/10 border-0  bg-layout-app-bg"
+            className="w-full rounded-sm px-3 py-2 text-left text-sm text-destructive-500 hover:bg-danger/10 border-0  bg-layout-app-bg"
             onClick={handleLogout}
           >
             Logout
