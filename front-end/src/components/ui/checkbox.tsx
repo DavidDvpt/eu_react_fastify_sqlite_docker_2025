864119ddafb41edab5867deb475909db27d05520
david mosca <david.mosca@pricehubble.com>
2026-06-28 10:43:53 +0200

    fix checkbox css

--- front-end/src/components/ui/checkbox.tsx
+++ front-end/src/components/ui/checkbox.tsx
@@ -11,13 +11,13 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-4 w-4 shrink-0 rounded-sm border border-black shadow-sm ring-offset-background focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-primary-500 data-[state=checked]:bg-primary-500",
+      "peer relative flex h-4 w-4 shrink-0 items-center justify-center rounded-sm border border-black bg-white shadow-sm ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-primary-500 data-[state=checked]:bg-primary-500",
       className,
     )}
     {...props}
   >
-    <CheckboxPrimitive.Indicator className="flex items-center justify-center text-white">
-      <Check className="h-3.5 w-3.5" />
+    <CheckboxPrimitive.Indicator className="absolute inset-0 flex items-center justify-center text-white translate-y-[1px]">
+      <Check className="h-4 w-4 stroke-[3]" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
