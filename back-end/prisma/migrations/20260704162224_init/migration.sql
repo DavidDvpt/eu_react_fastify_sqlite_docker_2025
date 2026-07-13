2193a7ae02707bff94675ccaf829598540c5cbda
david mosca <david.mosca@pricehubble.com>
2026-07-13 15:14:15 +0200

    fix migration

--- back-end/prisma/migrations/20260704162224_init/migration.sql
+++ back-end/prisma/migrations/20260704162224_init/migration.sql
@@ -2,4 +2,4 @@
 CREATE TYPE "PedCardTupleType" AS ENUM ('INITIAL_BALANCE', 'BUY_TTC', 'BUY_FEE', 'SELL_TTC', 'SELL_FEE', 'ADJUSTMENT');
 
 -- DropIndex
-DROP INDEX "pedcard_userId_idx";
+DROP INDEX IF EXISTS "pedcard_userId_idx";
