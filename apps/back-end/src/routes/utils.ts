907376dc9c837bce82265fa491b979a606bbc5bc
david mosca <david.mosca@pricehubble.com>
2026-08-01 15:58:33 +0200

    clean code

--- apps/back-end/src/routes/utils.ts
+++ apps/back-end/src/routes/utils.ts
@@ -1,12 +1,10 @@
 import type {} from '../types/fastify-augment.js';
 import type {} from '../types/fastify-jwt.js';
-import z from 'zod';
 
 import type { FastifyRequest } from 'fastify';
 
+import { requestUserSchema } from '#src/lib/schemas/commonSchemas.js';
+
 export function getRequestUserId(request: FastifyRequest): string {
-  const requestUserSchema = z.object({
-    id: z.string().min(1),
-  });
   return requestUserSchema.parse(request.user).id;
 }
